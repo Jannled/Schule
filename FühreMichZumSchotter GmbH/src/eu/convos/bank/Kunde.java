cf418bcf17552f5b8b8d145b048d1100b6eec86f
Jannled <jannled@live.de>
2019-06-26 10:13:57 +0200

    Fertiges Projekt Bank. Keine Garantie für Bugfreiheit xD

--- FühreMichZumSchotter GmbH/src/eu/convos/bank/Kunde.java
+++ FühreMichZumSchotter GmbH/src/eu/convos/bank/Kunde.java
@@ -4,30 +4,34 @@ public class Kunde
 {
 	public static int maxKonten = 4;
 	
-	private String name;
+	private String vorname;
+	private String nachname;
 	private Konto[] konten = new Konto[maxKonten];
 	
-	public Kunde(String name)
+	public Kunde(String vorname, String nachname)
 	{
-		this.name = name;
+		this.vorname = vorname;
+		this.nachname = nachname;
 	}
 	
-	public Kunde(String name, Konto konto)
+	public Kunde(String vorname, String nachname, Konto konto)
 	{
-		this.name = name;
+		this.vorname = vorname;
+		this.nachname = nachname;
 		konten[0] = konto;
 	}
 	
-	public Kunde(String name, Konto[] konten)
+	public Kunde(String vorname, String nachname, Konto[] konten)
 	{
-		this.name = name;
+		this.vorname = vorname;
+		this.nachname = nachname;
 		this.konten[0] = (konten.length > 0) ? konten[0] : null;
 		this.konten[1] = (konten.length > 1) ? konten[1] : null;
 		this.konten[2] = (konten.length > 2) ? konten[2] : null;
 		this.konten[3] = (konten.length > 3) ? konten[3] : null; //TODO Hardgecodet
 		
 		if(konten.length > 4)
-			System.err.printf("Der Kunde %s wurde mit %d Konten erstellt, nur die ersten vier wurden genommen!\n", name, konten.length);
+			System.err.printf("Der Kunde %s wurde mit %d Konten erstellt, nur die ersten vier wurden genommen!\n", getName(), konten.length);
 	}
 	
 	public Konto kontoAnlegen()
@@ -47,7 +51,23 @@ public class Kunde
 			return k;
 		}
 		
-		System.err.printf("Konnte kein Konto erstellen, die Maximale Anzahl von %d Konten wurde für %s erreicht\n", maxKonten, name);
+		System.err.printf("Konnte kein Konto erstellen, die Maximale Anzahl von %d Konten wurde für %s erreicht\n", maxKonten, getName());
+		return null;
+	}
+	
+	public Konto kontAnlegen(int kontoNummer)
+	{
+		for(int i=0; i<konten.length; i++)
+		{
+			if(konten[i] != null) continue;
+			
+			Konto k = new Konto(this, kontoNummer);
+			konten[i] = k;
+			System.out.printf("Erstelle neues %s\n", k.toString());
+			return k;
+		}
+		
+		System.err.printf("Konnte kein Konto erstellen, die Maximale Anzahl von %d Konten wurde für %s erreicht\n", maxKonten, getName());
 		return null;
 	}
 	
@@ -60,8 +80,26 @@ public class Kunde
 		return anz;
 	}
 	
+	public Konto getKonto(int index)
+	{
+		if(index <= anzKonten())
+			return konten[index];
+		
+		System.err.printf("%s besitzt kein Konto mit dem Index %d, er besitzt nur %d!", getName(), index, anzKonten());
+		return null;
+	}
+	
+	public void kontenAnzeigen()
+	{
+		for(int i=0; i<anzKonten(); i++)
+		{
+			System.out.printf("%" + (maxKonten/10 + 1) + "d %s %n", i, konten[i].toString());
+		}
+		System.out.println();
+	}
+	
 	public String getName()
 	{
-		return name;
+		return vorname + " " + nachname;
 	}
 }
--- FühreMichZumSchotter GmbH/src/eu/convos/bank/Main.java
+++ FühreMichZumSchotter GmbH/src/eu/convos/bank/Main.java
@@ -1,48 +1,166 @@
 package eu.convos.bank;
 
+import java.util.Scanner;
+
 public class Main 
 {
+	static Scanner scanner = new Scanner(System.in);
+	static Kunde[] kunden = new Kunde[100];
+	static int kundenAnzahl;
+	
+	public Main()
+	{
+		boolean running = true;
+		while(running) {
+			System.out.println();
+			System.out.println(" ------Hauptmenü------");
+			System.out.println("1 Kunde Wählen");
+			System.out.println("2 Kunde anlegen");
+			System.out.println("  Sonstige Eingaben beenden das Programm");
+			
+			String eingabe = scanner.next();
+			
+			switch (eingabe) {
+			//Kundenmenü
+			case "1":
+				kundenMenue();
+				break;
+				
+			//Neuen Kunden anlegen
+			case "2":
+				System.out.println("------Neuen Kunden anlegen------");
+				System.out.print("Bitte geben sie den Vornamen ein: ");
+				String vorname = scanner.next();
+				System.out.print("Bitte geben sie den Nachnamen ein: ");
+				kunden[kundenAnzahl++] = new Kunde(vorname, scanner.next());
+				break;
+
+			//Hauptmenü beenden
+			default:
+				running = false;
+			}
+		} 
+		
+	}
+	
+	public void kundenMenue()
+	{
+		boolean kundenSchleife = true;
+		
+		while(kundenSchleife)
+		{
+			System.out.println("  ------Kunden Wählen------");
+			for(int i=0; i<kundenAnzahl; i++)
+			{
+				System.out.printf("%2d %s %n", i, kunden[i].getName());
+			}
+			
+			int eingabe = -1;
+			while(eingabe < 0 || eingabe > kundenAnzahl)
+			{
+				eingabe = readInt();
+			}
+			
+			System.out.println(" ------" + kunden[eingabe].getName() + "------");
+			System.out.println("1 Konto wählen");
+			System.out.println("2 Konto eröffnen");
+			System.out.println("3 Konten anzeigen");
+			System.out.println("  Sonstige Eingaben verlassen das Kundenmenü.");
+			
+			switch (scanner.next()) {
+			//Kontomenü
+			case "1":
+				kontoMenue(kunden[eingabe]);
+				break;
+	
+			//Neues Konto erstellen
+			case "2":
+				System.out.println("Neues Konto erstellen!");
+				System.out.print("Bitte geben sie eine Kontonummer ein: ");
+				kunden[eingabe].kontoAnlegen(readInt());
+				break;
+				
+			//Alle Konten anzeigen
+			case "3":
+				kunden[eingabe].kontenAnzeigen();
+				break;
+				
+			//Konto Menü beenden
+			default:
+				kundenSchleife = false;
+			}
+		}
+	}
+	
+	public void kontoMenue(Kunde k)
+	{
+		System.out.printf("%" + (Kunde.maxKonten/10 + 1) + "d------%s------%n", k.getName());
+		k.kontenAnzeigen();
+		int wahlKonto = readInt();
+		while(wahlKonto < 0 || wahlKonto > k.anzKonten())
+		{
+			System.out.println("Ungültige Eingabe");
+		}
+		
+		boolean kontoSchleife = true;
+		while(kontoSchleife)
+		{
+			System.out.println(" ------Konto Menü------");
+			System.out.println("1 Einzahlung");
+			System.out.println("2 Abbuchung");
+			System.out.println("  Sonstige Eingaben beenden das Konto Menü");
+			
+			switch (scanner.next()) {
+			//Betrag einzahlen
+			case "1":
+				System.out.print("Geben sie den Betrag an, den sie einzahlen möchten: ");
+				k.getKonto(wahlKonto).einzahlen(readInt());
+				break;
+				
+			//Betrag auszahlen
+			case "2":
+				System.out.print("Geben sie den Betrag an, den sie abheben möchten: ");
+				k.getKonto(wahlKonto).abheben(readInt());
+				break;
+
+			//Konto Menü beenden
+			default:
+				kontoSchleife = false;
+			}
+		}
+	}
 	
 	public static void main(String[] args)
 	{
-		Kunde k = new Kunde("Lol");
-		Konto konto = k.kontoAnlegen(1000);
-		System.out.println(konto.toString());
-		konto.einzahlen(150);
-		konto.abheben(200);
-		konto.abheben(20);
-		System.out.println(konto.toString());
-		
-		
-		k.kontoAnlegen();
-		k.kontoAnlegen(100.225f);
-		k.kontoAnlegen();
-		
-		k.kontoAnlegen();
-		
-		Kunde k2 = new Kunde("Lel");
-		Konto konto2 = k2.kontoAnlegen(10.10f);
-		
-		konto.ueberweisen(konto2, 0);
-		konto.ueberweisen(konto2, 10);
-		konto.ueberweisen(konto2, 20);
-		konto.ueberweisen(konto2, 30);
-		konto.ueberweisen(konto2, 40);
-		konto.ueberweisen(konto2, 50);
-		konto.ueberweisen(konto2, 60);
-		konto.ueberweisen(konto2, 70);
-		konto.ueberweisen(konto2, 80);
-		konto.ueberweisen(konto2, 90);
-		konto.ueberweisen(konto2, 100);
-		konto.ueberweisen(konto2, 0);
-		
-		konto2.abheben(666.66f);
-		konto2.abheben(66.66f);
-		konto2.einzahlen(10);
-		
-		System.out.println("\n\n");
-		
-		Bankangestellter.letzteTransaktionen(konto);
-		Bankangestellter.letzteTransaktionen(konto2);
+		kunden[0]=new Kunde("Willi", "Winkler");
+        kunden[1]=new Kunde("Heinz", "Erwinkel");
+        kunden[2]=new Kunde("Peter", "Peters");
+        kundenAnzahl++;
+        kundenAnzahl++;
+        kundenAnzahl++;
+        
+        kunden[0].kontoAnlegen(7777);
+        kunden[1].kontoAnlegen(8888);
+        kunden[0].kontoAnlegen(9999);
+        
+        kunden[0].getKonto(0).einzahlen(100);
+        kunden[1].getKonto(0).abheben(100);
+        
+        new Main();
+        System.out.println("Beende Programm!");
+        scanner.close();
+	}
+	
+	/**
+	 * Frage so lange den Nutzer nach einer Eingabe bis dieser eine Zahl eintippt
+	 * @return Die eingegebene Zahl
+	 */
+	public static int readInt()
+	{
+		while(scanner.hasNext() && !scanner.hasNextInt())
+		{
+			System.out.println("\"" + scanner.next() + "\" ist keine gültige Zahl! ");
+		}
+		return scanner.nextInt();
 	}
 }
